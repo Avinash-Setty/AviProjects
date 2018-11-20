c80c37b4542359b5cbedb3ba20243871577b632e
OmniAsetty <36242674+OmniAsetty@users.noreply.github.com>
2018-11-20 17:44:37 -0500

    cleaning up

--- actions/types.js
+++
@@ -1,3 +0,0 @@
-export const PATTERNLIST_FETCH_SUCCESS = 'patternlist_fetch_success';
-export const PATTERNLIST_FETCH_FAIL = 'patternlist_fetch_fail';
-export const PATTERNLIST_FETCH_WAIT = 'patternlist_fetch_wait';

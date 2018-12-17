0ded5b909ae87ce4e4ea1592458edd9fcc5cc4a6
OmniAsetty <36242674+OmniAsetty@users.noreply.github.com>
2018-12-17 01:42:10 -0500

    Fixed the image component to display the image

--- SoftwarePatterns/components/common/CustomImage.js
+++ SoftwarePatterns/components/common/CustomImage.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, Dimensions, View } from 'react-native';
 
 export class CustomImage extends React.Component {
   render() {
       return (
-        <Image
-            style={[this.props.style, {flex:1, height: undefined, width: undefined}]}
-            source={this.props.source}
-            resizeMode="contain"
-            {...this.props}
-        />
+        <View style={{ justifyContent: 'center', flexDirection: 'row', padding: 5 }}>
+          <Image
+              style={[this.props.style, {height: 300, width: 300}]}
+              source={this.props.source}
+              resizeMode="contain"
+              {...this.props}
+          />
+        </View>
       );
   }
 }
--- SoftwarePatterns/screens/PatternDetailsScreen.js
+++ SoftwarePatterns/screens/PatternDetailsScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, ScrollView, StyleSheet } from 'react-native';
+import { View, ScrollView, StyleSheet, Image, Dimensions } from 'react-native';
 import { Title, Paragraph, CustomImage, SubTitle } from '../components/common';
 
 export default class PatternDetailsScreen extends React.Component {
